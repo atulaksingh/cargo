ca3c260ead0238705598323203bbece40aa04fc6
atulaksingh <talktoatulsingh@gmail.com>
2023-08-31 14:52:45 +0530

    s

--- src/components/Card/HeroCard.jsx
+++ src/components/Card/HeroCard.jsx
@@ -16,7 +16,7 @@ function HeroCard(props) {
         ></img>
         <div>
         {/* <div className=" absolute bg-red-500 bg-blend-saturation"></div> */}
-          <div className="absolute top-60 sm:top-28 lg:top-32 xl:top-80 mx-5 md:mx-28 lg:mx-40 xl:mx-60  2xl:mx-72">
+          <div className="absolute top-60 sm:top-28 lg:top-32 2xl:top-80 mx-5 md:mx-28 lg:mx-40 xl:mx-60  2xl:mx-72">
             <div className="container mx-auto">
               <div className="grid grid-cols-12">
                 <div className="col-span-12 2xl:col-span-7">
