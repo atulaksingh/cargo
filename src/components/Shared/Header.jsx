6bf6ad44b57876d7168c058808c50e43bc95e866
atulaksingh <talktoatulsingh@gmail.com>
2023-08-31 10:33:00 +0530

    updates

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
           </div>
           </div>
         </div> */}
-        <Navbar className="">
+        <Navbar className="bg-white">
           <Navbar.Brand>
             <img
               alt="logoimg"
@@ -39,7 +39,7 @@ function Header() {
 
             <Navbar.Toggle />
           </div>
-          <Navbar.Collapse className=" w-full h-[320px] md:h-full mx-auto bg-white">
+          <Navbar.Collapse className=" w-full h-[320px] md:h-full mx-auto">
             <Navbar className="font-bold text-base mx-auto py-5 hover:text-[#ff5e14] transition-all delay-75 ease-in-out cursor-pointer">
               Home
             </Navbar>
