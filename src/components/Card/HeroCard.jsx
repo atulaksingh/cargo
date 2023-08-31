cc6290b2220b416b7f6f49fa7d6c0569dcf384a0
atulaksingh <talktoatulsingh@gmail.com>
2023-08-31 14:30:38 +0530

    update

--- src/components/Card/HeroCard.jsx
+++ src/components/Card/HeroCard.jsx
@@ -11,8 +11,8 @@ function HeroCard(props) {
         <img
           src={props.img}
           alt="img"
-          // className="h-[38rem] sm:h-[42.2rem]  xl:h-[56.8999rem] w-full object-cover object-center"
-          className="h-screen w-full object-cover object-center"
+          className="h-[38rem] sm:h-[42.2rem] lg:h-[50rem]  xl:h-[56.8999rem] w-full object-cover object-center"
+          // className="h-screen w-full object-cover object-center"
         ></img>
         <div>
         {/* <div className=" absolute bg-red-500 bg-blend-saturation"></div> */}
